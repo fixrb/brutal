7c6f9b6fd29985ebb7b4b7f2aa859e2a43c7c062
Cyril Kato <contact@cyril.email>
2019-12-26 22:43:32 +0100

    feat(ScaffoldGenerator): add rspec

--- examples/hello_world/test.rb
+++ examples/hello_world/test.rb
@@ -6,11 +6,13 @@ front_object = "Hello "
 # ------------------------------------------------------------------------------
 
 actual = front_object + 'Alice'
+
 raise unless actual.to_s == "Hello Alice"
 raise unless actual.length == 11
 
 # ------------------------------------------------------------------------------
 
 actual = front_object + 'Bob'
+
 raise unless actual.to_s == "Hello Bob"
 raise unless actual.length == 9
--- examples/some_array_concatenation/test.rb
+++ examples/some_array_concatenation/test.rb
@@ -1,5 +1,6 @@
 # Some array concatenation unit tests
 
+
 # ------------------------------------------------------------------------------
 
 front_object = [42, Object]
@@ -7,35 +8,41 @@ front_object = [42, Object]
 # ------------------------------------------------------------------------------
 
 actual = front_object + [nil, "!"]
+
 raise unless actual.to_s == "[42, Object, nil, \"!\"]"
 raise unless actual.length == 4
 
 # ------------------------------------------------------------------------------
 
 actual = front_object + [nil, '!!']
+
 raise unless actual.to_s == "[42, Object, nil, \"!!\"]"
 raise unless actual.length == 4
 
 # ------------------------------------------------------------------------------
 
 actual = front_object + [nil, "..."]
+
 raise unless actual.to_s == "[42, Object, nil, \"...\"]"
 raise unless actual.length == 4
 
 # ------------------------------------------------------------------------------
 
 actual = front_object + [1, "!"]
+
 raise unless actual.to_s == "[42, Object, 1, \"!\"]"
 raise unless actual.length == 4
 
 # ------------------------------------------------------------------------------
 
 actual = front_object + [1, '!!']
+
 raise unless actual.to_s == "[42, Object, 1, \"!!\"]"
 raise unless actual.length == 4
 
 # ------------------------------------------------------------------------------
 
 actual = front_object + [1, "..."]
+
 raise unless actual.to_s == "[42, Object, 1, \"...\"]"
 raise unless actual.length == 4
