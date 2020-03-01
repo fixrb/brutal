30a7a28694547709208ce8c514ccc7023bae7b9b
Cyril Kato <contact@cyril.email>
2020-03-01 16:59:01 +0100

    test(brutal): reach 100% of coverage

--- test.rb
+++ test.rb
@@ -5,12 +5,26 @@ require 'simplecov'
 ::SimpleCov.command_name 'Brutal test suite'
 ::SimpleCov.start
 
-require './lib/brutal'
+require './lib/brutal/scaffold'
 
 # ------------------------------------------------------------------------------
 
 actual = begin
-  Brutal::Scaffold.new("require './lib/brutal/scaffold'", '"Hello %{context_string}"', *"%{subject}.to_s", context_string: ["Alice", "Bob"])
+  ::Brutal::Scaffold.new("# frozen_string_literal: true", '"Hello " + "%{string}"', *"%{subject}.length", **{:string=>["Alice", "Bob"]})
 end
 
-raise if actual.to_s != "require './lib/brutal/scaffold'\n\n# ------------------------------------------------------------------------------\n\nactual = begin\n  \"Hello Alice\"\nend\n\nraise if actual.to_s != \"Hello Alice\"\n\n# ------------------------------------------------------------------------------\n\nactual = begin\n  \"Hello Bob\"\nend\n\nraise if actual.to_s != \"Hello Bob\"\n"
+raise if actual.blank_line != "\n# ------------------------------------------------------------------------------\n\n"
+raise if actual.context_names != [:string]
+raise if actual.test_params != [["Alice"], ["Bob"]]
+raise if actual.to_s != "# frozen_string_literal: true\n\n# ------------------------------------------------------------------------------\n\nactual = begin\n  \"Hello \" + \"Alice\"\nend\n\nraise if actual.length != 11\n\n# ------------------------------------------------------------------------------\n\nactual = begin\n  \"Hello \" + \"Bob\"\nend\n\nraise if actual.length != 9\n"
+
+# ------------------------------------------------------------------------------
+
+actual = begin
+  ::Brutal::Scaffold.new("# frozen_string_literal: true", '"Hello " + "%{string}"', *"%{subject}.to_s", **{:string=>["Alice", "Bob"]})
+end
+
+raise if actual.blank_line != "\n# ------------------------------------------------------------------------------\n\n"
+raise if actual.context_names != [:string]
+raise if actual.test_params != [["Alice"], ["Bob"]]
+raise if actual.to_s != "# frozen_string_literal: true\n\n# ------------------------------------------------------------------------------\n\nactual = begin\n  \"Hello \" + \"Alice\"\nend\n\nraise if actual.to_s != \"Hello Alice\"\n\n# ------------------------------------------------------------------------------\n\nactual = begin\n  \"Hello \" + \"Bob\"\nend\n\nraise if actual.to_s != \"Hello Bob\"\n"
